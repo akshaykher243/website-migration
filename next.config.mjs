6e94b548cde2cc9f0416c3103254ac1421f2a52d
akshaykher243 <akshay.kher@techholding.co>
2025-03-10 16:52:29 +0530

    fix: removed the sonarqube and other as well

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@payload-config': path.resolve(__dirname, './src/payload.config.ts'),
+    }
+    return config
+  },
 }
 
 export default withPayload(nextConfig)
